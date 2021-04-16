72401e2626cb596e4b90bb038490d865e016c60d
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-04-16 15:05:47 -0400

    finished creating the server.js

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
 });
 
 // routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/api.js"));
+app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/htmlRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
