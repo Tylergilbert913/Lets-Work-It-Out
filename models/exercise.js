2473320c9cc4aa3c90a10eb44270c5e467177ea0
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-04-16 15:45:11 -0400

    creating the exercise model

--- models/exercise.js
+++ models/exercise.js
@@ -3,20 +3,41 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const exerciseSchema = new Schema({
-  name: {
+  day: {
+    type: Date,
+    default: Date.now(),
+  },
+  exercises: [
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: "Enter Exercise Name"
+    },
     type: String,
     trim: true,
-    required: "Enter a name for transaction"
+    required: "Enter Exercise Type"
   },
-  value: {
+  distance : {
+    type: Number
+  },
+  duration : {
     type: Number,
-    required: "Enter an amount"
+    required: "Enter Exercise Duration"
   },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+  weight : {
+    type: Number
+  },
+  sets : {
+    type: Number
+  },
+  reps : {
+    type: Number
+  },
+  ]
 });
+
+
 
 const Excercise = mongoose.model("Transaction", exerciseSchema);
 
