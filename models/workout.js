13006c837bbfafd2014fe24e5ece11e47321f2f7
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-04-19 14:15:09 -0400

    added virtual function to models

--- models/workout.js
+++ models/workout.js
@@ -1,46 +1,55 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now(),
-  },
-  exercises: [
-    {
-      name: {
-        type: String,
-        trim: true,
-        required: "Enter Your Exercise Name"
-      },
-      type: {
-        type: String,
-        trim: true,
-        required: "Enter Your Exercise Type"
-      },
-      distance: {
-        type: Number
-      },
-      duration: {
-        type: Number,
-        required: "Enter Your Exercise Duration"
-      },
-      weight: {
-        type: Number
-      },
-      sets: {
-        type: Number
-      },
-      reps: {
-        type: Number
-      },
-    }
-  ]
+const WorkoutSchema = new Schema({
+    day: {
+      type: Date,
+      default: Date.now
+    },
+    exercises: [
+        {
+          type: {
+            type: String,
+            trim: true,
+            required: true
+          },
+          name: {
+            type: String,
+            trim: true,
+            required: true
+          },
+          duration: {
+            type: Number,
+            required: true
+          },
+          weight: {
+            type: Number
+          },
+          reps: {
+            type: Number
+          },
+          sets: {
+            type: Number
+          },
+          distance: {
+            type: Number
+          }
+      }
+    ]
+  },{
+  toJSON: {
+    virtuals: true
+  }
+  });
+
+// This adds a dynamically-created property to schema
+WorkoutSchema.virtual("totalDuration").get(function () {
+  // This will "reduce" array of exercises down to just the sum of their durations
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
-
-
-
-const Workout = mongoose.model("workout", workoutSchema);
-
-module.exports = Workout;
+  
+  const Workout = mongoose.model("workout", WorkoutSchema);
+  
+  module.exports = Workout;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,10 +7,12 @@ module.exports = function (app) {
     app.get("/", function (req, res) {
         res.sendfile(path.join(__dirname, "../public/index.html"));
     });
+
     // exercise page
     app.get("/exercise", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
+    
     // stats page
     app.get("/stats", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/stats.html"));
