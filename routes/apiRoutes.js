4ff08b41ca2c82d4f26dd1a392e5366dfaac346e
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-04-19 00:02:43 -0400

    decided to format everything and use express sent it was preinstalled with our packages

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,39 +1,40 @@
+const router = require("express").router();
 const db = require("../models");
 
-module.exports = function(app) {
-    // App.get to pull up info for the workouts page
-    app.get("/api/workouts", (req, res) => {
-        db.Workout.find({}).then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+module.exports = function (app) {
+  // App.get to pull up info for the workouts page
+  router.get("/api/workouts", (req, res) => {
+    db.Workout.find({}).then(dbWorkout => {
+      res.json(dbWorkout);
     })
-    // App.get to pull up info for the range page
-    app.get("/api/workouts/range", ({}, res) => {
-      db.Workout.find({}).then((dbWorkout) => {
-        res.json(dbWorkout);
-      }).catch(err => {
+      .catch(err => {
         res.status(400).json(err);
       });
+  })
+  // App.get to pull up info for the range page
+  router.get("/api/workouts/range", ({ }, res) => {
+    db.Workout.find({}).then((dbWorkout) => {
+      res.json(dbWorkout);
+    }).catch(err => {
+      res.status(400).json(err);
     });
-    // App.post to submit new completed workouts
-    app.post("/api/workouts/", (req, res) => {
-        db.Workout.create(req.body).then((dbWorkout) => {
-          res.json(dbWorkout);
-        }).catch(err => {
-            res.status(400).json(err);
-          });
-      });
-      // App.put to update workouts by MongoDB _id value and update the exercsise body
-      app.put("/api/workouts/:id", (req, res) => {
-        db.Workout.findByIdAndUpdate(
-          { _id: req.params.id }, { exercises: req.body }
-        ).then((dbWorkout) => {
-          res.json(dbWorkout);
-        }).catch(err => {
-          res.status(400).json(err);
-        });
+  });
+  // App.post to submit new completed workouts
+  router.post("/api/workouts/", (req, res) => {
+    db.Workout.create(req.body).then((dbWorkout) => {
+      res.json(dbWorkout);
+    }).catch(err => {
+      res.status(400).json(err);
     });
+  });
+  // App.put to update workouts by MongoDB _id value and update the exercsise body
+  router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+      { _id: req.params.id }, { exercises: req.body }
+    ).then((dbWorkout) => {
+      res.json(dbWorkout);
+    }).catch(err => {
+      res.status(400).json(err);
+    });
+  });
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,10 +2,16 @@ const path = require("path");
 
 module.exports = function (app) {
 
+
+    // home page
+    app.get("/", function (req, res) {
+        res.sendfile(path.join(__dirname, "../public/index.html"));
+    });
+    // exercise page
     app.get("/exercise", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/exercise.html"));
     });
-
+    // stats page
     app.get("/stats", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/stats.html"));
     });
